df377c57d5a17c90703b54d9acc8b7da593654f5
Cletus <cletus6@gmail.com>
2020-12-03 22:37:33 -0500

    add click animation for buttons in javascript

--- 3-rotating-navigation/index.js
+++ 3-rotating-navigation/index.js
@@ -6,3 +6,7 @@ const open = document.getElementById('open');
 open.addEventListener('click', () => {
 	container.classList.add('show-nav');
 });
+
+close.addEventListener('click', () => {
+	container.classList.remove('show-nav');
+});
